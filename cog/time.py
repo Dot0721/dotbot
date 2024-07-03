4d73ac1c9d4f6543e2dbfbf100a84fa3d5e85319
Dot0721 <dot.point.spot@gmail.com>
2024-07-03 22:50:01 +0800

    set time

--- cog/time.py
+++ cog/time.py
@@ -10,11 +10,20 @@ class Time(commands.Cog):
         self.bot = bot
         self.channel = None
         self.bg_task = self.bot.loop.create_task(self.interval())
-    async def interval(self):
+    '''async def interval(self):
         await self.bot.wait_until_ready()
         self.channel = self.bot.get_channel(int(jdata['set_time_channel']))
         while not self.bot.is_closed():
             await self.channel.send("I'm working!!!")
-            await asyncio.sleep(5)
+            await asyncio.sleep(5)'''
+    async def time_task(self):
+        await self.bot.wait_until_ready()
+        self.channel = self.bot.get_channel(int(jdata['set_time_channel']))
+        while not self.bot.is_closed():
+            pass
+    async def set_time(self,ctx,time):
+        jdata['time'] =time
+        with open('setting.json', mode='w', encoding='utf8') as jfile:
+            json.dump(jdata,jfile,indent=4)
 async def setup(bot: commands.Bot):
     await bot.add_cog(Time(bot))
